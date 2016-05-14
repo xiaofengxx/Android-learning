2411c2e52d493664de25183d96aa27ade9be9a30
xiaofeng <651908051@qq.com>
2016-05-14 15:58:19 +0800

    仿写一个简单的VIewpager

---
+++ Class-02/app/src/main/java/com/myviewpager/hasika/myviewpager/MainActivity.java
@@ -0,0 +1,19 @@
+package com.myviewpager.hasika.myviewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyView myView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myView = (MyView) findViewById(R.id.myview);
+
+
+    }
+}
---
+++ Class-02/app/src/main/java/com/myviewpager/hasika/myviewpager/MyView.java
@@ -0,0 +1,23 @@
+package com.myviewpager.hasika.myviewpager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by HaSiKa on 2016/3/9.
+ */
+public class MyView extends ViewGroup{
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
